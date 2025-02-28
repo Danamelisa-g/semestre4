c391d54d3978c4abb56f0e18f6d3f9bfb329fff1
Danamelisa-g <danamelisag17@gmail.com>
2025-02-28 16:49:05 -0500

    arreglo

--- ejerciciodan.js
+++ ejerciciodan.js
@@ -1,10 +1,12 @@
-const valorMaximo = Math.max(...Array)
+const array = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+const valorMaximo = Math.max(...array)
 console.log(valorMaximo)
 //contar donde se encuentra el numero
 
 const listaDeMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
-const masConMayorVentas = Array.indexof(valorMaximo)
+const masConMayorVentas = array.indexOf(valorMaximo)
 console.log(masConMayorVentas)
 
 const mes = listaDeMeses[masConMayorVentas]
